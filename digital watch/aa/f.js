dcea2f8971fb42191a53a39481384cdcf4d728fe
odanroterif <143523077+odanroterif@users.noreply.github.com>
2024-03-15 16:09:49 -0300

    Update f.js

--- digital watch/aa/f.js
+++ digital watch/aa/f.js
@@ -1,15 +1,20 @@
 const hour = document.querySelector('#hours');
 const minute = document.querySelector('#minutes');
 const second = document.querySelector('#seconds');
+const picture = document.querySelector('#picture');
+const message = document.querySelector('#message');
+const DaTe = document.querySelector('#DaTe');
 
 setInterval(clock,1000);
+setInterval(screen_switch,1000);
 
-
+var day;
+var actual_hour;
 
 function clock()
 {
-    let day = new Date();
-    let actual_hour = day.getHours();
+    day = new Date();    
+    actual_hour = day.getHours();
     let actual_minute = day.getMinutes();
     let actual_second = day.getSeconds();
     
@@ -17,20 +22,61 @@ function clock()
     {
         actual_hour = '0' + actual_hour;
     } 
-
+    
     hour.textContent = actual_hour;
-
+    
     if (actual_minute < 10) 
     {
         actual_minute = '0' + actual_minute;
     }
-
+    
     minute.textContent = actual_minute;
-
+    
     if (actual_second < 10) 
     {
         actual_second = '0' + actual_second;
     }
     
     second.textContent = actual_second;
+}
+
+function screen_switch()
+{    
+    let actual_date = day.getDate();
+    let actual_mounth = day.getMonth() + 1;
+    let actual_year = day.getFullYear();
+    
+    
+    if (actual_date < 10) 
+    {
+        DaTe.textContent = '0' + actual_date + '/' + actual_mounth + '/' + actual_year;
+    } 
+    else 
+    {
+        DaTe.textContent = actual_date + '/' + '0' + actual_mounth + '/' + actual_year;
+    }
+    
+    if (actual_mounth < 10) 
+    {
+        DaTe.textContent = actual_date + '/' + '0' + actual_mounth + '/' + actual_year;
+    } 
+    else 
+    {
+        DaTe.textContent = actual_date + '/' + '0' + actual_mounth + '/' + actual_year;
+    }
+    
+
+    if(actual_hour > 12 && actual_hour < 18)
+    {
+        message.textContent = "BOA TARDE";     
+        picture.src = 'images/tarde.avif';
+    } else if (actual_hour >= 5 && actual_hour <= 12) 
+    {
+        picture.src = 'images/dia.jpg';
+        message.textContent ="BOM DIA";
+    } else 
+    {
+        picture.src = 'images/noite.jpg';
+        message.textContent = "BOA NOITE";
+    }
 }
