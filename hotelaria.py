ee54c34f9bcd117fc8b0fc6066e8d3d4edb72a3d
odanroterif <143523077+odanroterif@users.noreply.github.com>
2024-04-16 10:33:16 -0300

    Create hotelaria.py

---
+++ hotelaria.py
@@ -0,0 +1,12 @@
+Foi solicitado um sistema para gerenciar pedidos de hospedagem, esse sistema devi registrar dados importantes de verificação de hospedes como o seu nome, idade, o registro próprio
+do hospede e email esclarecer dúvidas, um cliente só poderá reservar um único quarto em seu nome, os quartos devem ser identificados pelo andar que estão e o número do quarto.
+Para os funcionários deve-se ter  o id do funcionário e seu cargo que definira que permissões o funcionário terá para manipular as reservas.
+
+nesse sistema deve ter dicas de pontos turisticos, diferença de pacotes, diferença de acesso entre usuario, funcionario, adm geral. de acordo com o pedido da empresa, esse 
+sistema deve ter relação com internet, ser de facil uso, ter relação com a região. deve ter explicação se acomoda carro ou não.
+
+o adm geral é responsável por colocar o salario na conta de cada funcionário de acordo com suas funções, ter acesso total aos registros do banco mas não pode excluir nem alterar 
+as reservas, cada reserva tem em suas informações a informação do quarto em que foi designado, o tipo de serviço solicidado pelo hospede, as informações do hospede bem como a 
+data de criação da reserva e o prazo que o cliente tem antes da reserva expirar.
+
+passado o prazo de tolerância de atraso a reserva devera ser excluida e o quarto devera constar como disponivel.
